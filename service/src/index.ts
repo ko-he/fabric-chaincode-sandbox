a10d9b1a50f5fb71c091271fbea9ed54f7ec43a1
ko-he <localhost.ko@gmail.com>
2020-01-04 19:55:10 +0900

    set up typescript

---
+++ service/src/index.ts
@@ -0,0 +1,7 @@
+import * as Express from 'express'
+const app = Express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
